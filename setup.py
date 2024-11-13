c5dbc482d68d298272cd795ec68373260dd2b040
caaszj <773598627@qq.com>
2024-11-13 10:51:54 +0800

    imp

--- setup.py
+++ setup.py
@@ -1,33 +1,34 @@
 from setuptools import setup, find_packages
 
-# Runtime Requirements
-inst_reqs = [
-        "gdal",
+dependencies = [
+  "numpy>=1.19.5",
+  "gdal",
 ]
 
 setup(
     name="opengis",
-    version="3.0.5",
+    version="3.0.6",
     description="An open source GIS tool.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="Zijie Wang",
     author_email="773598627@qq.com",
     packages=find_packages(),
-    install_requires=inst_reqs,
-    python_requires=">=3.10",
+    install_requires=dependencies,
+    python_requires = ">=3.9",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
-        "Programming Language :: Python :: 3.13",
     ],
-    keywords="GIS Remote-sensing",
+    keywords="GIS",
     project_urls={
-        "Source":"https://github.com/caaszj/opengis",
-        "Tracker":"https://github.com/caaszj/opengis/issues"
+        'Source':"https://github.com/caaszj/opengis",
+        'Tracker':"https://github.com/caaszj/opengis/issues"
         }
 )
